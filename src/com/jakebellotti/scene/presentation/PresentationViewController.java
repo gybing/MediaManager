e1f6e4bee10b2269870ab9e263ebb39b36f494c7
Jake Bellotti <jake.bellotti@gmail.com>
2016-03-21 22:15:32 +0800

    updated with ControlFX custom search bar

--- src/com/jakebellotti/scene/presentation/PresentationViewController.java
+++ src/com/jakebellotti/scene/presentation/PresentationViewController.java
@@ -2,6 +2,8 @@ package com.jakebellotti.scene.presentation;
 
 import java.io.File;
 
+import org.controlsfx.control.textfield.TextFields;
+
 import com.jakebellotti.model.MovieFile;
 import com.jakebellotti.model.MovieListOrderer;
 
@@ -14,6 +16,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 /**
  * 
@@ -22,111 +26,127 @@ import javafx.scene.image.ImageView;
  */
 
 public class PresentationViewController {
-	
+
+	@FXML
+	private AnchorPane root;
+
 	@FXML
 	private ListView<MovieFile> movieList;
 	/**
-	 * Allows the user to select how they want to view the movie list, for example text with a thumbnail or just text.
+	 * Allows the user to select how they want to view the movie list, for
+	 * example text with a thumbnail or just text.
 	 */
-    @FXML
-    private ComboBox<?> viewTypeComboBox;
-    /**
-     * Where the user can type in a search query to further filter the movies.
-     */
-    @FXML
-    private TextField searchTextField;
-    /**
-     * Resets or gets rid of all of the search filters applied to this movie list.
-     */
-    @FXML
-    private Button resetFiltersButton;
-    /**
-     * Opens up the filter management screen so the user can apply or remove filters.
-     */
-    @FXML
-    private Button manageFiltersButton;
-    /**
-     * Where the user can select how they want to order their movie list by.
-     */
-    @FXML
-    private ComboBox<MovieListOrderer> orderByComboBox;
-    /**
-     * Moves up one in the movie list.
-     */
-    @FXML
-    private Button upMoviesButton;
-    /**
-     * Moves down one in the movie list.
-     */
-    @FXML
-    private Button downMoviesButton;
-    /**
-     * A picture of the movie cover.
-     */
-    @FXML
-    private ImageView moviePoster;
-    /**
-     * The label at the top of the screen that has the movie name and release year
-     */
-    @FXML
-    private Label movieNameLabel;
-    /**
-     * Displays all the movie information like runtime, release date, rating etc.
-     */
-    @FXML
-    private Label movieInfoLabel;
-    /**
-     * A list view that displays all of the actors featured in this movie.
-     */
-    @FXML
-    private ListView<String> movieActorListView;
-    /**
-     * A button the user presses to manually modify the data retrieved.
-     */
-    @FXML
-    private Button manuallyModifyDataButton;
-    /**
-     * A button the user presses to automatically retrieve the movie definition.
-     */
-    @FXML
-    private Button retrieveDataButton;
-    /**
-     * A text area displaying the plot of the movie.
-     */
-    @FXML
-    private TextArea moviePlotTextArea;
-    /**
-     * A button that will clear the user's input in the search box.
-     */
-    @FXML
-    private Button clearSearchButton;
-    /**
-     * A button the user presses to open the movie in their selected program.
-     */
-    @FXML
-    private Button openMovieButton;
-    /**
-     * A label displaying the writer and director of the movie
-     */
-    @FXML
-    private Label movieAuthorLabel;
-    /**
-     * Displays how 
-     */
-    @FXML
-    private Label automatedDataLabel;
+	@FXML
+	private ComboBox<?> viewTypeComboBox;
+	/**
+	 * Where the user can type in a search query to further filter the movies.
+	 */
+	@FXML
+	private TextField searchTextField;
+	/**
+	 * Resets or gets rid of all of the search filters applied to this movie
+	 * list.
+	 */
+	@FXML
+	private Button resetFiltersButton;
+	/**
+	 * Opens up the filter management screen so the user can apply or remove
+	 * filters.
+	 */
+	@FXML
+	private Button manageFiltersButton;
+	/**
+	 * Where the user can select how they want to order their movie list by.
+	 */
+	@FXML
+	private ComboBox<MovieListOrderer> orderByComboBox;
+	/**
+	 * Moves up one in the movie list.
+	 */
+	@FXML
+	private Button upMoviesButton;
+	/**
+	 * Moves down one in the movie list.
+	 */
+	@FXML
+	private Button downMoviesButton;
+	/**
+	 * A picture of the movie cover.
+	 */
+	@FXML
+	private ImageView moviePoster;
+	/**
+	 * The label at the top of the screen that has the movie name and release
+	 * year
+	 */
+	@FXML
+	private Label movieNameLabel;
+	/**
+	 * Displays all the movie information like runtime, release date, rating
+	 * etc.
+	 */
+	@FXML
+	private Label movieInfoLabel;
+	/**
+	 * A list view that displays all of the actors featured in this movie.
+	 */
+	@FXML
+	private ListView<String> movieActorListView;
+	/**
+	 * A button the user presses to manually modify the data retrieved.
+	 */
+	@FXML
+	private Button manuallyModifyDataButton;
+	/**
+	 * A button the user presses to automatically retrieve the movie definition.
+	 */
+	@FXML
+	private Button retrieveDataButton;
+	/**
+	 * A text area displaying the plot of the movie.
+	 */
+	@FXML
+	private TextArea moviePlotTextArea;
+	/**
+	 * A button the user presses to open the movie in their selected program.
+	 */
+	@FXML
+	private Button openMovieButton;
+	/**
+	 * A label displaying the writer and director of the movie
+	 */
+	@FXML
+	private Label movieAuthorLabel;
+	/**
+	 * Displays how
+	 */
+	@FXML
+	private Label automatedDataLabel;
 
-    @FXML
-    private Button automatedDataButton;
+	@FXML
+	private Button automatedDataButton;
 
-    @FXML
-    private Label fileInfoLabel;
+	@FXML
+	private Label fileInfoLabel;
 
 	@FXML
 	public void initialize() {
 		File file = new File("./data/testimg.jpg");
 		Image image = new Image(file.toURI().toString());
 		moviePoster.setImage(image);
+		setupSearchTextField();
+	}
+
+	private void setupSearchTextField() {
+		this.root.getChildren().remove(this.searchTextField);
+
+		final TextField newSearchTextField = TextFields.createClearableTextField();
+		newSearchTextField.setLayoutX(this.searchTextField.getLayoutX());
+		newSearchTextField.setLayoutY(this.searchTextField.getLayoutY());
+		newSearchTextField.setPrefWidth(this.searchTextField.getPrefWidth());
+
+		this.searchTextField = newSearchTextField;
+		this.root.getChildren().add(this.searchTextField);
 	}
 
 }
